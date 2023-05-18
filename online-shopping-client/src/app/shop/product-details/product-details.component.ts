ffcb3ef287e8b6439e08c63fe0cfb3bdd800bea0
BojanaSamardzioska <b_samardzioska@yahoo.com>
2023-05-19 01:54:08 +0200

    Improved UI, added breadcrumbs, loading spinner and carousel.

--- online-shopping-client/src/app/shop/product-details/product-details.component.ts
+++ online-shopping-client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +14,10 @@ export class ProductDetailsComponent implements OnInit {
   
   product?: Product;
 
-  constructor(private route: ActivatedRoute, private shopService: ShopService) {}
+  constructor(private route: ActivatedRoute, private shopService: ShopService,
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@productDetails', ' ');
+    }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -23,7 +27,10 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     if(id) {
       this.shopService.getProduct(+id).subscribe({
-        next: response => this.product = response,
+        next: response => {
+          this.product = response;
+          this.bcService.set('@productDetails', this.product.name);
+        },
         error: error => console.log(error)
       });  
     }
